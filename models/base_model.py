ada8944b16a9ea626af23de59b9fd153fb3d43ba
Dennis Kamau <dkamau476@gmail.com>
2023-02-06 13:28:14 -0800

    pycodstyle correction

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,8 @@ class BaseModel:
         self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}"\
+            .format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute
